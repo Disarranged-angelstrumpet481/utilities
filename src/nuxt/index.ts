a252bd58cbdb06ff06b2374e5896b54ad168068e
selcukcukur <selcukcukur@outlook.com.tr>
2026-04-01 01:20:34 +0300

    nuxt(constants): introduce app constants for env and configuration

--- src/nuxt/index.ts
+++ src/nuxt/index.ts
@@ -1,2 +1,11 @@
+// Core (Constants & Functions)
 export * from "../core"
+
+// Vue (Constants & Functions)
 export * from "../vue"
+
+// Constants
+export * from "./constants"
+
+// Functions
+// export * from "./functions"
