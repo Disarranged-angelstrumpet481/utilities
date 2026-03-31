acfa2efeec185e3375021e410a869de4391e5ac3
selcukcukur <selcukcukur@outlook.com.tr>
2026-04-01 01:54:17 +0300

    feat(constants): add country to calling code mapping

--- src/core/constants/index.ts
+++ src/core/constants/index.ts
@@ -2,3 +2,7 @@
 export * from "./configuration/blacklist"
 export * from "./configuration/localhost"
 export * from "./configuration/reserved"
+
+// Localization
+export * from "./localization/currency"
+export * from "./localization/phone"
