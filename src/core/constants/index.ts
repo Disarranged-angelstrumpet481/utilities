577b2ef7d100c132bef847422361c055829798a9
selcukcukur <selcukcukur@outlook.com.tr>
2026-04-01 02:05:12 +0300

    feat(constants): add ISO region codes and mappings

--- src/core/constants/index.ts
+++ src/core/constants/index.ts
@@ -3,6 +3,12 @@ export * from "./configuration/blacklist"
 export * from "./configuration/localhost"
 export * from "./configuration/reserved"
 
+// Geography
+export * from "./geography/continents"
+export * from "./geography/countries"
+export * from "./geography/regions"
+
 // Localization
 export * from "./localization/currency"
+export * from "./localization/domain"
 export * from "./localization/phone"
